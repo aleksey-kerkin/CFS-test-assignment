dd408480df93eb897606c7f29e402b190fef1f5b
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2025-03-26 11:05:46 +0500

    Add base UI components (Input, Button)

---
+++ src/shared/ui/Button.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  disabled?: boolean;
+}>();
+</script>
+
+<template>
+  <button :disabled="disabled" class="bg-zinc-700 px-2 py-4 text-white/80">
+    <slot />
+  </button>
+</template>
---
+++ src/shared/ui/Input.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  modelValue: string;
+}>();
+
+const emit = defineEmits<{
+  (e: "update:modelValue", value: string): void;
+}>();
+
+const handleInput = (e: Event) => {
+  emit("update:modelValue", (e.target as HTMLInputElement).value);
+};
+</script>
+
+<template>
+  <input
+    type="text"
+    :value="modelValue"
+    @input="handleInput"
+    ,
+    class="mb-4 w-full p-2"
+    placeholder="Search posts..."
+  />
+</template>
