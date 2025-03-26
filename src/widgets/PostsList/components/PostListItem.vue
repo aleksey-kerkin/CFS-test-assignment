facdd336a11fd8775e4558fa2613cd74e00fdfeb
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2025-03-26 11:35:19 +0500

    Create PostListItem with navigation functionality

---
+++ src/widgets/PostsList/components/PostListItem.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Post } from "@/entities/post/types";
+import { useRouter } from "vue-router";
+
+const props = defineProps<{
+  post: Post;
+}>();
+
+const router = useRouter();
+
+const navigateToPost = () => {
+  router.push(`/post/${props.post.id}`);
+};
+</script>
+
+<template>
+  <div
+    class="my-4 cursor-pointer rounded-lg border border-zinc-500 p-4 duration-200 hover:bg-zinc-400"
+    @click="navigateToPost"
+  >
+    <h3>{{ post.title }}</h3>
+    <p>{{ post.body }}</p>
+  </div>
+</template>
